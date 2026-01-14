c515d6cac98cf8ab8b3372e1a030505e622f475c
teacherantonkrupinski-wq <teacherantonkrupinski@gmail.com>
2026-01-14 16:06:49 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Basic Service Worker for offline support (optional)
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+self.addEventListener('fetch', event => {
+  // You can add caching strategies here
+});
